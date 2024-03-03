38c4a87b8f120e230ad491a6aedaa537d115b9ce
MATHS-IS-FUN <86098178+MATHS-IS-FUN@users.noreply.github.com>
2024-03-04 12:38:26 +1300

    Initial commit

---
+++ src/Student_Code.c
@@ -0,0 +1,37 @@
+/**
+ * @file Student_Code.c
+ * @author your name (you@domain.com)
+ * @brief description of this file
+ * @version 0.1
+ * @date yyyy-mm-dd
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+/* Libraries. DO NOT REMOVE */
+#include "main.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include "Student_Code.h"
+
+// ---------------------- Defining physical robot parameters --------------------------
+// Update these numbers to match the physical robot (information found in the lab manual)
+int drivingWheelDiameter = 0;	    // diameter of the driving wheels [mm]
+int robotWidth = 0;					// width of the robot including the wheel thickness [mm]
+int wheelWidth = 0;					// width of the driving wheel [mm]
+double drivingWheelRatio = 0.0;	    // ratio of wheel shaft rotations to wheel motor shaft rotations
+double armRatio = 0.0;				// ratio of arm shaft rotations to arm motor shaft rotations
+double encCountPerRev = 0.0;	    // number of encoder ticks per 1 revolution of the motor shaft
+// ------------------------------------------------------------------------------------
+
+/* Write your code in the function below. You may add helper functions below the studentCode function. */
+void student_Main()
+{
+    
+}
+
+// ----------------------------------------------- Function defintions go here  -----------------------------------------------//
+// Don't forget to add your function prototypes to Student_Code.h
---
+++ src/Student_Code.h
@@ -0,0 +1,14 @@
+#ifndef STUDENT_CODE_H
+#define STUDENT_CODE_H
+
+void student_Main();    // The main entry point to the student code
+
+// Add your function prototypes below
+
+
+
+
+
+
+// DO NOT ADD ANY PROTOTYPES AFTER THIS LINE
+#endif  // STUDENT_CODE_H
